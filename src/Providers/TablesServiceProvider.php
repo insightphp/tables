e06ad86958896013d6f31b3ef2a0042017d3910d
Peter Stovka <stovka.peter@gmail.com>
2022-08-28 18:58:48 +0200

    init

---
+++ src/Providers/TablesServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Insight\Tables\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class TablesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
